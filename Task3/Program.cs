d3bee3b2912491a6db5402ccaaed9ace78f66e48
Ekaterina <kat-erina-07@mail.ru>
2023-06-21 10:47:10 +0400

    Table of cubes

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23. Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+*/
+int InputNum(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+int number = InputNum("Введите целое число: ");
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    if (i < number)
+        Console.Write(", ");
+    else
+        Console.Write(".");
+}
